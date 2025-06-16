b993af3d9641f0e08913ed12f4e1d8f2391bd48a
jeperico <joaoeperico@gmail.com>
2025-06-16 12:15:53 -0300

    [fix] remove [] to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: ['class'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
